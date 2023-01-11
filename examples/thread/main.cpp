35a027e5ce4706ad29560d36b8fb7838bd2ac44e
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2023-01-11 10:47:39 +0100

    Add examples

---
+++ examples/thread/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+
+void worker() {
+	std::cout << "I'm worker()\n";
+}
+
+void worker1(int val) {
+	std::cout << "I'm worker(" << val << ")\n";
+}
+
+struct X {
+	void operator()() {
+		std::cout << "X operator()\n";
+	}
+	void run(int val) {
+		std::cout << "X run(" << val << ")\n";
+	}
+};
+
+void work_ref(int& val) {
+	std::cout << "I'm work_ref(" << val << ")\n";
+}
+int main() {
+	X x;
+	int a = 12;
+	std::thread thr1{worker};
+	std::thread thr2{worker1, 42};
+	std::thread thr3{[](float val){ std::cout << "I'm lambda("
+		<< val << ")\n";
+	}, 82};
+	std::thread thr4{x};
+	std::thread thr5{&X::run, &x, 78};
+	std::thread thr6;
+	std::thread thr7{work_ref, std::ref(a)};
+	thr6 = std::move(thr7);
+
+	for(auto & t: {&thr1, &thr2, &thr3, &thr4, &thr5, &thr6, &thr7}) {
+		if(t->joinable()) {
+			t->join();
+		}
+	}
+}
+
